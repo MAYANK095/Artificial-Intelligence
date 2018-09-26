c5b9d0f8a298e54128d3ef1b36c6d4d8fda63db2
Mayank Mittal <mittalmayank095@gmail.com>
2018-09-27 00:51:51 +0530

    To Display The Graph Using MatPlotLib

---
+++ one1.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+x=[1,2,3] #x axis values
+y=[4,5,1] #y axis values
+plt.plot(x,y) #to plot
+plt.xlabel('X axis') #to name x axis
+plt.ylabel('y axis') #to name y axis
+plt.title('Graph One') #to name the graph
+plt.show() #to display the graph
+
